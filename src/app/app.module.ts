98b3c66434bfc14facbcd58bb46951d8c798aec3
Hyakun <xdanyro1@gmail.com>
2022-07-18 23:29:52 +0300

    project

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog'
+
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AddItemFormComponent } from './components/add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './components/budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './components/budget-item-card/budget-item-card.component';
+import { EditItemModelComponent } from './components/edit-item-model/edit-item-model.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent,
+    EditItemModelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  providers: [],
+  entryComponents:[EditItemModelComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
